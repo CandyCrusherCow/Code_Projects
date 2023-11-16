de83fb33b472702c89adacdf59c3de65250c43b4
Eveyn <eveyn.akow@gmail.com>
2023-11-15 20:35:33 -0500

    Dictionary app updates

--- Desktop/homework/personal/Coding/Code_Projects/DictionaryApp/js/main.js
+++ Desktop/homework/personal/Coding/Code_Projects/DictionaryApp/js/main.js
@@ -7,6 +7,14 @@ let word =document.querySelector('input').value
 .then(res => res.json())
 .then (data => {console.log(data[0])
  document.querySelector('h2').innerText = data[0].word
+ console.log(data[0].meanings[0].definitions)
+ data[0].meanings.forEach(obj => {
+    const li = document.createElement('li')
+    li.textContent = obj.definitions.forEach(def =>{const li = document.createElement('li')
+    li.textContent = def.definition
+    document.querySelector('ul').appendChild(li)})
+    document.querySelector('ul').appendChild(li)
+ });
 //  document.querySelector('img'). src = data.drinks[0].strDrinkThumb
 // document.querySelector('h3').innerText = data.drinks[0].strInstructions
 })
