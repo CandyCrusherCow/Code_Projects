345df6011fde16d05aea5df4f945cfd4f538d3eb
Eveyn <127455728+EvelynAkow@users.noreply.github.com>
2023-11-24 23:35:34 -0500

    Sorting Cards

---
+++ Desktop/homework/personal/Coding/Code_Projects/SortingCards/main.js
@@ -0,0 +1,43 @@
+// Write a function sort_cards() that sorts a shuffled list of cards, so that any given list of cards is sorted by rank, no matter the starting collection.
+
+// All cards in the list are represented as strings, so that sorted list of cards looks like this:
+
+// ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+
+
+function sortCards(array){
+    for(let i = 0;i<array.length;i++){
+      if(array[i]=='A'){
+    array[i]= 1}
+      else if(array[i]== 'T'){
+        array[i] = 10
+      }
+      else if(array[i]== 'J'){
+    
+    array[i] = 11
+      }else if(array[i]== 'Q'){
+    
+    array[i] = 12
+      }else if(array[i]== 'K'){
+    
+    array[i] = 13}
+      }
+      array.sort((a,b)=> a-b)
+      for(let i = 0;i<array.length;i++){
+      if(array[i]==1){
+    array[i]= 'A'}
+      else if(array[i]== 10){
+        array[i] = 'T'
+      }
+      else if(array[i]== 11){
+    
+    array[i] = 'J'
+      }else if(array[i]== 12){
+    
+    array[i] = 'Q'
+      }else if(array[i]== 13){
+    
+    array[i] = 'K'}
+      }
+      return array
+      }
