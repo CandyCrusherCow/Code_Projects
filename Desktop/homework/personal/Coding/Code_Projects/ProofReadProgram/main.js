de42ead94f1ceaf55a647955f4e24e0162c3c864
Eveyn <127455728+EvelynAkow@users.noreply.github.com>
2023-12-11 15:32:28 -0500

    ProofReadProgram

---
+++ Desktop/homework/personal/Coding/Code_Projects/ProofReadProgram/main.js
@@ -0,0 +1,12 @@
+
+    //You've just finished writing the last chapter for your novel when a virus suddenly infects your document. It has swapped the 'i's and 'e's in 'ei' words and capitalised random letters. Write a function which will:
+    
+    //a) remove the spelling errors in 'ei' words. (Example of 'ei' words: their, caffeine, deceive, weight)
+    
+    //b) only capitalise the first letter of each sentence. Make sure the rest of the sentence is in lower case.
+    
+    //Example: He haD iEght ShOTs of CAffIEne. --> He had eight shots of caffeine.
+
+function proofread (str) { 
+    return str.toLowerCase().replaceAll('ie','ei').split('. ').map((string)=>string.charAt(0).toUpperCase()+string.slice(1)).join('. ')
+    }
